aa086af0c30bca51f318ebdd9391e5505f0ec953
Marcelo Magalhães <marcelolours@gmail.com>
2021-10-26 22:19:03 -0300

    introCSS

---
+++ Aula 5/funcaoSimples.js
@@ -0,0 +1,5 @@
+function converterpoledas (x){
+    var pol = 0.39;
+    console.log(x*pol);
+}
+converterpoledas(10);
---
+++ Aula 5/stringToURL.js
@@ -0,0 +1,6 @@
+function converteURl (u){
+  console.log("http://wwww." + x + ".com.br");
+}
+
+var x = 'Google';
+converteURl(x);
