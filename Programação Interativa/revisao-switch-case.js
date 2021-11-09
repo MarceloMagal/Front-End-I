d88f7f1bb53510e9114e0a8deab2568ebc83db30
Marcelo Magalhães <marcelolours@gmail.com>
2021-11-09 19:54:40 -0300

    Checkpoint ProImp

---
+++ Programação Interativa/revisao-switch-case.js
@@ -0,0 +1,44 @@
+function qualMes(valor) {
+    switch (valor) {
+        case 1:
+            console.log('Janeiro');
+            break;
+        case 2:
+            console.log('Fevereiro');
+            break;
+        case 3:
+            console.log('Março');
+            break;
+        case 4:
+            console.log('Abril');
+            break;
+        case 5:
+            console.log('maio');
+            break;
+        case 6:
+            console.log('Janeiro');
+            break;
+        case 1:
+            console.log('Janeiro');
+            break;
+        case 1:
+            console.log('Janeiro');
+                                        break;
+                                        case 1:
+                                            console.log('Janeiro');
+                                            break;
+                                            case 1:
+                                                console.log('Janeiro');
+                                                break;
+                                                case 1:
+                                                    console.log('Janeiro');
+                                                    break;
+                                                    case 1:
+                                                        console.log('Janeiro');
+                                                        break;
+                                                                                                                                                                                                                                                                            
+        default:
+            break;
+    }
+}
+qualMes(3);
