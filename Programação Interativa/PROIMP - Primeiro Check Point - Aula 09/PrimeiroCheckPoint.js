c6ede805afcf2db469c213433d0551fd917e01a9
Marcelo Magalhães <marcelolours@gmail.com>
2021-11-10 15:14:31 -0300

    CheckPoint PROIMP final

--- Programação Interativa/PROIMP - Primeiro Check Point - Aula 09/PrimeiroCheckPoint.js
+++ Programação Interativa/PROIMP - Primeiro Check Point - Aula 09/PrimeiroCheckPoint.js
@@ -22,26 +22,32 @@ function qualprato(escolha, tempo) {
         break;
 
         default:
+            padrao= 10000;
+            tempo = 13000;
             console.log('Prato inexistente');
         break;
     }    
 
-    if ((escolha = 'Pipoca') || (escolha = 'Macarrão') (escolha = 'Carne') || (escolha = 'Feijão') (escolha = 'Brigadeiro') ) {
-        
+    if ( (escolha = 'Pipoca') || (escolha = 'Macarrão') || (escolha = 'Carne') || (escolha = 'Feijão') || (escolha = 'Brigadeiro') ) {
+        passa = 1;
     
         if (tempo === padrao) {            
             console.log('Prato pronto, bom apetite!!!') } else {
+
             if (tempo < padrao) {            
                 console.log('tempo insuficiente') } else {    
-                if ((tempo >= (padrao*2)) && (tempo < (padrao*3))) {
+
+                if ((tempo >= (padrao*2)) && (tempo < (padrao*3))) { 
                     console.log('a comida queimou') } else {
-                    if (tempo >= (padrao*3)) {
+                        
+                    if ((tempo >= (padrao*3)) && (passa = 1) ) {
                         console.log('Kabuumm') 
                     }
                 }
-            };               
+            }               
         }
 
      }
 }
-qualprato ('Carne', 30);
+
+qualprato ('Fejão', 1);
