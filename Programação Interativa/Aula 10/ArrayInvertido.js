190bd4e1ab640c1ba9070c7a08fed38c7e3a942a
Marcelo Magalhães <marcelolours@gmail.com>
2021-11-14 11:55:29 -0300

    1CheckPoint PROIMP final

---
+++ Programação Interativa/Aula 10/ArrayInvertido.js
@@ -0,0 +1,13 @@
+
+
+let str = ['1', '2', '3' ];
+function inverterArray () {
+
+   let novoarray = [ str[2], str[1] , str[0] ];
+
+
+    console.log(novoarray);
+    
+};
+inverterArray();
+
---
+++ Programação Interativa/Aula 10/somarArray.js
@@ -0,0 +1,8 @@
+let meuarray = [10, 3, 10, 4]; 
+function somarArray(params) {
+    
+    console.log(meuarray[0] + meuarray[1] + meuarray[2] + meuarray[3]   );
+    
+}
+
+somarArray();
